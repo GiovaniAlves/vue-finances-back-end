1809f47143f946084bfa7b517f882e6d98692f97
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-02 17:41:10 -0300

    Implementado a query accounts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,25 @@
 const { getUserId } = require('./../utils')
 
+function accounts(_, args, context, info) {
+
+   const userId = getUserId(context)
+   return context.db.query.accounts({
+      where: {
+         OR: [
+            {
+               user: {
+                  id: userId
+               }
+            },
+            {
+               user: null
+            }
+         ]
+      },
+      orderBy: 'description_ASC'
+   }, info)
+}
+
 function user(_, args, context, info) {
 
    const userId = getUserId(context)
@@ -7,5 +27,6 @@ function user(_, args, context, info) {
 }
 
 module.exports = {
+   accounts,
    user
 }
