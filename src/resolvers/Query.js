b39244a4801ef738faab2d2cb17278a58539363b
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-03 21:48:46 -0300

    Implementado a mutation que retorna o total dos lançamentos filtrando por data

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -79,6 +79,40 @@ function records(_, {type, accountsIds, categoriesIds, month}, context, info) {
 }
 
 
+
+function totalBalance(_, { date }, context, info) {
+
+   const userId = getUserId(context)
+   const dateIso = moment(date, 'YYYY-MM-DD').endOf('day').toISOString()
+   const pgSchema = `${process.env.PRISMA_SERVICE}$${process.env.PRISMA_STAGE}`
+
+   const mutation = `
+      mutation TotalBalance($database: PrismaDatabase, $query: String!) {
+         executeRaw(database: $database, query: $query)
+      }
+   `
+   const variables = {
+      database: 'default',
+      query: `
+         SELECT SUM("${pgSchema}"."Record"."amount") as totalbalance
+            FROM "${pgSchema}"."Record"
+            
+            INNER JOIN "${pgSchema}"."_RecordToUser"
+            ON "${pgSchema}"."_RecordToUser"."A" = "${pgSchema}"."Record"."id"
+            
+            WHERE "${pgSchema}"."_RecordToUser"."B" = '${userId}'
+            AND "${pgSchema}"."Record"."date" <= '${dateIso}'
+      `
+   }
+
+   return context.prisma.$graphql(mutation, variables)
+      .then( response => {
+         const totalBalance = response.executeRaw[0].totalbalance
+         return totalBalance ? totalBalance : 0
+      })
+}
+
+
 function user(_, args, context, info) {
 
    const userId = getUserId(context)
@@ -89,5 +123,6 @@ module.exports = {
    accounts,
    categories,
    records,
+   totalBalance,
    user
 }
